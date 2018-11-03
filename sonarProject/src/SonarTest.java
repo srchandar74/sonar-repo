6ed2a250af0bdf8a2224db35b634b738482c58c6
srchandar74 <31071104+srchandar74@users.noreply.github.com>
2018-11-03 22:59:59 +0100

    Create SonarTest.java

---
+++ sonarProject/src/SonarTest.java
@@ -0,0 +1,21 @@
+package sonarProject;
+
+
+
+public class SonarTest {
+	
+	static int sub(int x, int y){
+		int s=x-y;
+		return s;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Sonar Test Exam-ple");
+		int m=sub(5, 2);
+		System.out.println("subtraction is: "+ m);
+
+	}
+
+}
